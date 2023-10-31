996607af5075dc90c957764406e7a66c3904dd20
grongierisc <guillaume.rongier@intersystems.com>
2023-10-31 17:19:39 +0100

    add pagination

--- app/config/settings.py
+++ app/config/settings.py
@@ -71,12 +71,16 @@ INSTALLED_APPS = [
     'rest_framework'
 ]
 
+TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
+
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20
 }
 
 MIDDLEWARE = [
