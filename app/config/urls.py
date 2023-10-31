59eb1c41cea5641eabac8c35a76d80a8590b77ef
grongierisc <guillaume.rongier@intersystems.com>
2023-10-31 17:20:13 +0100

    add template

--- app/config/urls.py
+++ app/config/urls.py
@@ -16,16 +16,32 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
-from main.views import CommunityCommentViewSet, CommunityPostViewSet, CommunityTagViewSet
+from main.views import CommunityCommentViewSet, CommunityPostViewSet, CommunityTagViewSet, IrisDemoViewSet
+from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 router = routers.DefaultRouter()
 router.register(r'CommunityComment', CommunityCommentViewSet)
 router.register(r'CommunityPost', CommunityPostViewSet)
 router.register(r'CommunityTag', CommunityTagViewSet)
+router.register(r'IrisDemo', IrisDemoViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('openapi/', get_schema_view(
+        title="Post and Comment API",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
+    path('redoc/', TemplateView.as_view(
+        template_name='redoc.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='redoc'),
 
 ]
